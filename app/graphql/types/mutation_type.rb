8d0bd954deca11255a05c0182ddd0e7287c95db1
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2024-02-21 12:09:15 +0530

    remove unused code

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,9 +3,6 @@ module Types
     field :create_user, mutation: Mutations::CreateUser
     field :update_user, mutation: Mutations::UpdateUser
     field :delete_user, mutation: Mutations::DeleteUser
-    # field :create_comment, Types::CommentType, mutation: Mutations::CreateComment
-    # field :update_comment, Types::CommentType, mutation: Mutations::UpdateComment
-    # field :delete_comment, Types::CommentType, mutation: Mutations::DeleteComment
     field :create_post, Types::PostType, mutation: Mutations::CreatePost
     field :update_post, Types::PostType, mutation: Mutations::UpdatePost
     field :delete_post, Types::PostType, mutation: Mutations::DeletePost
